bafe304da232d9d1d48a99eb197742ea1929914a
fu-jw <1151242757@qq.com>
2023-07-15 10:59:15 +0800

    Update cf_worker.js
    修改URL，https://chimeragpt.adventblocks.cc

--- cf_worker.js
+++ cf_worker.js
@@ -1,4 +1,4 @@
-const TELEGRAPH_URL = 'https://api.openai.com';
+const TELEGRAPH_URL = 'https://chimeragpt.adventblocks.cc';// 修改URL，https://chimeragpt.adventblocks.cc，原值：https://api.openai.com
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
